20e48003b0b6afb63c7868ac1c4e19a9d3b2f998
angtamrod <167316585+angtamrod@users.noreply.github.com>
2024-07-02 13:59:10 +0200

    creación de funciones para servir ficheros estáticos

--- index.js
+++ index.js
@@ -5,11 +5,34 @@ const {createServer} = require("http"); //creamos un servidor con http createSer
 
 const {createReadStream} = require("fs");
 
+function contentType(extension){//Esto explica los tipos de archivo que soporta el servidor que estamos creando
+    if(extension == "html") return "text/html";//Si solo hacemos una linea de if se hace solo en una linea sin llaves
+    if(extension == "css") return "text/css";
+    if(extension == "js") return "text/javascript";
+    if(extension == "jpg" || extension == "jpeg") return "image/jpeg";
+    if(extension == "png") return "image/png";
+    if(extension == "json") return "application/json";
+    return "text/plain"; 
+}
+
+function servirFichero(respuesta, ruta, tipo, status){
+    respuesta.writeHead(status, {"Content-type" : tipo });
+    
+    let fichero = createReadStream(ruta); //creamos un flujo de lectura
+    fichero.pipe(respuesta);//Con esto conectamos el fichero a la respuesta para que fluyan los datos
+    fichero.on("end", () => {
+        respuesta.end(); //Presiona el enter para que viaje al cliente
+    }); //Cuando termines de fluir invoca este callback
+}
+
 const servidor = createServer((peticion, respuesta) => {
 
-    respuesta.writeHead(404, { "Content-type" : "text/html" }); //Es una función que me va a permitir crear las cabeceras y configurar la respuesta.
-    respuesta.write("..x cosa");//Al no poner una imagen aquí no funciona
-    respuesta.end();
+    // respuesta.writeHead(404, { "Content-type" : "text/html" }); //Es una función que me va a permitir crear las cabeceras y configurar la respuesta.
+    // respuesta.write("..x cosa");//Al no poner una imagen aquí no funciona
+    // respuesta.end();
+
+    //Invocamos la funcion de arriba porque es un método más rápido para hacer lo que hemos hecho antes
+    servirFichero(respuesta, "./publico/index.html", contentType("html"), 200);
 });
 
 servidor.listen(process.env.PORT || 3000); // Cuando no estemos en render usaremos este puerto, pero en render nos proporcionarán uno que no controlaremos
